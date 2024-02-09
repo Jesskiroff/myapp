19442d49b70050d082d6f1fb92067cb18beede5c
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-09 17:06:47 -0500

    trying out css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name = "Jessica";
+const lName = "Kiroff"
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+{/*const numb = 7; */}
+
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <div>
+
+   {/*<h1>Hello {name} {lname}!</h1>*/}
+   {/*<h1>Hello {name + " " + lName}</h1>*/}
+    <h1 className="heading">Created by {`${name} ${lName}`} </h1>
+    {/*one can add any expression into curly braces but not statements (such as if statements) */}
+    <p>Your lucky number is {Math.floor(Math.random() * 5)} </p>
+    <p>Copyright {year}</p>
+  </div>,
   document.getElementById("root")
 );
